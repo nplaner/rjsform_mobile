3b09259ba06d215f0605dcf98d8c28dad89b78c0
Hang Xu <hangxu199371@gmail.com>
2021-07-14 11:59:07 -0700

    ported over from rjsform2

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import Form from "@rjsf/core";
 
-export default function App() {
+const schema = {
+  title: "Data Surge LLC Employee Form",
+  type: "object",
+  properties: {
+    name: {
+      type: 'string'
+    },
+    age: {
+      type: 'number'
+    },
+    role: {
+      type: 'string'
+    },
+    tasksDone: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          title: { type: 'string' },
+          description: { type: 'string' }
+        }
+      }
+    }
+  }
+};
+// const ArrayFieldTemplate = (props) => {
+//   return (
+//     <div>
+//       {props.items.map(element => element.children)}
+//       {props.canAdd && <button type="button" onClick={props.onAddClick}>Add new task</button>}
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const [lat, setLat] = useState(0)
+  const [lon, setLon] = useState(0)
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude)
+      setLon(position.coords.longitude)
+    })
+  }, [lat, lon])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Form 
+      schema={schema}
+      // ArrayFieldTemplate={ArrayFieldTemplate}
+      onSubmit={({formData}) => alert(JSON.stringify(formData, null, 2))}
+      />
+      <Text>Current longitude and latitude: {lon}, {lat}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
